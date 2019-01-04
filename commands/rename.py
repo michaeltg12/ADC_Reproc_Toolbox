157733c5962ffa8419c3300dd7b9c660d80bb66c
Michael Giansiracusa <giansiracumt@ornl.gov>
2019-01-04 09:43:54 -0500

    fixed some help messages

--- commands/rename.py
+++ commands/rename.py
@@ -1,15 +1,16 @@
 import click
-
 from logging import getLogger
 reproc_logger = getLogger('reproc_logger')
 
-@click.command(help='This will rename some fucking files! Yea!')
-@click.option('--indexes', '-i', default='', help='The fucking indexes to cut! Fuck yea!') # this has to be the last
-def rename(indexes):
+@click.command('rename', help='This will rename some files! Yea!')
+@click.option('--indexes', '-i', default='', help='The indexes to cut!') # this has to be the last
+@click.pass_obj
+def rename(config, indexes):
+    reproc_logger.info(config)
     reproc_logger.info('indexes = {}'.format(indexes))
-    reproc_logger.info('rename some shitty files.')
+    reproc_logger.info('rename some files.')
     printStuff()
 
-@click.command(help='print stuff help')
+@click.command('printStuff', help='print stuff help')
 def printStuff():
     click.echo('testing import method .... idk stuff')
